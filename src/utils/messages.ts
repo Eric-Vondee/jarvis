a445ffedd7017b749c97299f95156760f33aa3d8
Eric <ericvondee5@gmail.com>
2023-08-22 01:01:54 +0100

    update welcome message

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -1,9 +1,14 @@
 export const initMessage = `
-Gm, 
+Hello!👋
 
-I am Jarvis your personal assistant 
+I am Jarvis, I am here to help you access the reasonable information about any token on these networks BSC, CELO, ETH, POLYGON with just the token CONTRACT ADDRESS in 1 second. 
 
-## Commands 
-/help: view instructions
-/search: search for token and returns information
+You will get to know about the information of token, like, Name  - Pool symbol - Total Supply -  Total trades - Daily Transactions - Dext score. Etc
+
+Getting Started!
+
+❇️ Commands
+
+/help: to view instructions
+/search: to search and access token information in 1 sec
 `;
